48458b32dac1c73484eedf2414d4f8efcc60232a
Akky <akshaydp07@gmail.com>
2018-08-06 01:27:47 +0530

    added initial structure plus changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # PetClinicApp
-this is pet clinic app developed using spring boot by akshay
+this is pet clinic app developed using spring boot by akshay paturkar
--- src/main/java/com/petclinic/petclinicapp/PetclinicappApplication.java
+++ src/main/java/com/petclinic/petclinicapp/PetclinicappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetclinicappApplication {
 
     public static void main(String[] args) {
+        System.out.println("inside main class");
         SpringApplication.run(PetclinicappApplication.class, args);
     }
 }
